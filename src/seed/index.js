f91ce62d3428d96ae864a3304f61802e2ed29560
dpinkus87 <dylanpinkus@gmail.com>
2023-04-24 11:14:07 -0500

    updated seed index.js

--- src/seed/index.js
+++ src/seed/index.js
@@ -1,59 +1,59 @@
-import React, { UseState } from "react";
+import React, { useState } from "react";
 
-const productList = ([category]) => {
-    const [product] = UseState([
+const ProductList = ({ category }) => {
+  const [products] = useState([
+    {
+      name: "AA01",
+      category: "ring",
+      image: "",
+    },
+    {
+      name: "AA02",
+      category: "ring",
+      image: "",
+    },
+    {
+      name: "BB01",
+      category: "necklaces",
+      image: "",
+    },
+    {
+      name: "BB02",
+      category: "necklaces",
+      image: "",
+    },
+    {
+      name: "CC01",
+      category: "bracelets",
+      image: "",
+    },
+    {
+      name: "CC02",
+      category: "bracelets",
+      image: "",
+    },
+  ]);
 
-        {
-            name: 'AA01',
-            category: 'ring',
-            image: ''
-        },
-        {
-            name: 'AA02',
-            category: 'ring',
-            image: ''
-        },
-        {
-            name: 'BB01',
-            category: 'necklaces',
-            image: ''
-        },
-        {
-            name: 'BB02',
-            category: 'necklaces',
-            image: ''
-        },
-        {
-            name: 'CC01',
-            category: 'bracelets',
-            image: ''
-        },
-        {
-            name: 'CC02',
-            category: 'bracelets',
-            image: ''
-        },
+  const currentProducts = products.filter((product) => product.category === category);
 
-    ]
-    )
+  // const [currentProduct, setCurrentProduct] = useState(false);
 
-    const currentProducts = product.filter(product => product.category === category);
-
-    // const [currentProduct, setCurrentProduct] = seState(false);
-
-    return (
-        <div>
-            <div className="flex-row">
-                {currentProducts.map((image, i) => (
-                    <img
-                        src={require(`../../src/Images/${category}/${i}.jpeg`)}
-                        alt={image.name}
-                        className="img-thumbnail mx-1"
-                        key={image.name} />
-                ))}
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div className="flex-row">
+        {currentProducts.map((product, i) => (
+          <img
+            src={require(`../../src/Images/${category}/${i}.jpeg`).default}
+            alt={product.name}
+            className="img-thumbnail mx-1"
+            key={product.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
-export default productList;
+export default ProductList;
+
+
