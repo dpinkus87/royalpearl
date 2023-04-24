abf77fce274d5dddf08c1252971f8685bf1ead99
dpinkus87 <dylanpinkus@gmail.com>
2023-04-24 10:26:37 -0500

    updated app.js routes, added noMatch page

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import { Route, Router, Routes } from "react-router";
 import "./App.css";
 
 import Home from "./pages/Home";
-// import ProductListing from "./pages/ProductListing";
-// import Regal from "./pages/Regal";
+import ProductListing from "./pages/ProductListing";
+import Regal from "./pages/Regal";
+import noMatch from './pages/NoMatch'
 
 import Header from "./components/Navigation/index";
 import Footer from "./components/Footer";
@@ -13,12 +14,31 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Header />
+        <Routes>
+          <Route
+            path='/'
+            element={<Home />}
+          />
+          <Route
+          path='/regal'
+          element={<Regal />}
+          />
+          <Route
+            path='/ProductListing'
+            element={<ProductListing />}
+          />
+          <Route
+          path='*'
+          element={<NoMatch />}
+/>
+        </Routes>
 
-      <Header />
 
-      <Home />
+        <Footer />
+      </Router>
 
-     <Footer />
 
     </div>
 
