8eb81211af0404b2e856720ea4b6162ed5e768e9
dpinkus87 <dylanpinkus@gmail.com>
2023-04-26 11:14:17 -0500

    created addToMessage util

--- src/pages/ProductListing/index.js
+++ src/pages/ProductListing/index.js
@@ -1,31 +1,31 @@
-// // import productCard from "../../components/ProdCard/index";
-// import { useEffect } from "react";
-// // import { ProductList } from '../../seed';
-// import { Container } from "react-bootstrap";
+import productCard from "../../components/ProdCard/index";
+import { useEffect } from "react";
+import { ProductList } from '../../seed';
+import { Container } from "react-bootstrap";
 
 
-// const styles = {
-//     gridlayout: {
-//         border: 'none'
-//     }
-// }
-// const ProductListing = () => {
-//     useEffect(() => {
-//         // get product
-//     })
+const styles = {
+    gridlayout: {
+        border: 'none'
+    }
+}
+const ProductListing = () => {
+    useEffect(() => {
+        // get product
+    })
 
-//     return (
-//         <Container>
-//             <div style={styles.gridlayout}>
-//                 {ProductList.map((item) => {
-//                     return (<productCard key={item.id} id={item.id} />)
-//                 })}
-//             </div>
-//         </Container>
-//     )
-// }
+    return (
+        <Container>
+            <div style={styles.gridlayout}>
+                {ProductList.map((item) => {
+                    return (<productCard key={item.id} id={item.id} />)
+                })}
+            </div>
+        </Container>
+    )
+}
 
-// export default ProductListing
+export default ProductListing
 
 
 
--- src/utils/addToMessage.js
+++ src/utils/addToMessage.js
@@ -1,18 +1,73 @@
+import React, { useState, useEffect } from 'react';
+import { ProductList } from '../seed/index';
 
-const addToMessage = (arr) => {
-    let id;
-
-    if (arr.length) {
-        const ids = arr.map((e) => e.id)
-        ids.sort((a, b) => a - b)
-        id = ids[ids.length = 1] + 1;
-
-    } else {
-        id = 1;
-    }
-
-    return id;
+const ProductsContext = React.createContext();
+export function UseProducts() {
+  return UseProducts(ProductsContext);
 }
 
+const messageHandler = ({ children }) => {
+  const [products, setProducts] = useState(ProductList);
+  const [message, setMessage] = useState([]);
+  const [messageEmpty, setMessageEmpty] = useState(true);
 
-export default addToMessage;
+  const addMessage = (product, e) => {
+    e.preventDefault();
+    const existingProductIndex = message.findIndex(item => item.id === product.id);
+    handleMessage(product);
+    if (existingProductIndex < 0) {
+      const updatedMessage = [...message, { ...product, quantity: 1, messaged: true }];
+      setMessage(updatedMessage);
+      localStorage.setItem('productsInMessage', JSON.stringify(updatedMessage));
+    } else {
+      if (message.length === 1) {
+        localStorage.setItem('productsInMessage', JSON.stringify([]));
+        const updatedMessage = JSON.parse(localStorage.getItem('productsInMessage'));
+        setMessage(updatedMessage);
+      } else {
+        message.splice(existingProductIndex, 1);
+        localStorage.setItem('productsInMessage', JSON.stringify(message));
+        const updatedMessage = JSON.parse(localStorage.getItem('productsInMessage'));
+        setMessage(updatedMessage);
+      }
+    }
+  };
+
+
+  function checkMessage(list, products) {
+    if(list.length === 0){
+        products.forEach(product => product.messaged = false)
+    }else{
+        for (let i = 0, i < products.length; i++){
+            for (let j = 0; j < list.length; j++) {
+                if (products[i].id === list[j].id) {
+                    products[i].messaged = true;
+                    break;
+                }else{
+                    products[i].messaged = false 
+                }
+            }
+        }
+    }
+  }
+  return([...products])
+
+}
+
+  const handleMessage = (product) => {
+    setMessage(prevState => prevState.map(item => {
+      if (item.id === product.id) {
+        return { ...item, messaged: !item.messaged};
+      }
+      return item;
+    }));
+  };
+
+  return (
+    <ProductsContext.Provider value={{ products, message, messageEmpty, setMessage, setProducts, addMessage }}>
+      {children}
+    </ProductsContext.Provider>
+  );
+};
+
+export default messageHandler;
