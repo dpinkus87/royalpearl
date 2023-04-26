0b4945affb6502888fa94246b28ee976956a8838
dpinkus87 <dylanpinkus@gmail.com>
2023-04-26 10:03:53 -0500

    some formatting on Modal

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import {Col, Row} from 'react-bootstrap'
 
 function HomepageModal() {
   const [show, setShow] = useState(false);
@@ -12,23 +13,57 @@ function HomepageModal() {
     handleShow(); // call handleShow on component mount
   }, []);
 
+  const styles = {
+    body: {
+        background: 'black',
+        textSize: "50px",
+        color: 'white',
+        radius: '5%'
+    },
+    col1: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRight: '5px',
+        borderColor: 'white'
+    },
+    col2: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+}
+
   return (
     <>
      
 
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
+      <Modal show={show} onHide={handleClose} aria-labelledby="contained-modal-title-vcenter"
+      centered>
+       
+        <Modal.Body  style={styles.body}>
+        <Row>
+            <Col style={styles.col1}>
+            <div>
+                About Royal Pearl
+            </div>
+            <Button variant="secondary" onClick={handleClose}>
+            Royal Pearl
           </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
+        </Col>
+        
+          <Col style={styles.col2}>
+          <div>
+             About Regal Regal
+          </div>
+           <Button variant="secondary" onClick={handleClose}>
+Regal           </Button>
+          </Col>
+        </Row>
+
+        </Modal.Body>
+        
+      
       </Modal>
     </>
   );
